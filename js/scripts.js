5539368083459bd071e8e53929f61f23ea7abb98
luciano <melo.esteban@gmail.com>
2022-05-29 00:56:31 -0300

    tickets

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+const nombre = document.getElementById('#nombre');
+const apellido = document.querySelector('#apellido');
+const correo = document.querySelector('#correo');
+const categoria = document.querySelector('#categoria');
+const cantidad = document.querySelector('#cantidad');
+
+const form = document.getElementById('form');
+
+
+
+form.addEventListener('submit' , function(event){
+    event.preventDefault();
+    console.log(nombre +" " + apellido +" " + correo +" " + categoria + " "+ cantidad);
+    console.log(form);
+    let total = (cantidad.value * categoria.value * 200) / 100;
+    console.log(total);
+    document.getElementById('total').textContent = total;
+
+});
+
+document.getElementById("btnBorrar").addEventListener("click", function(event){
+    form.reset();
+});
